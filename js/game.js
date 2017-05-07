67191af56b0176d8d472c775f8e0adcb05a1c57b
jiangtao123456 <1449565208@qq.com>
2017-05-07 16:35:23 +0800

    Update game.js

--- js/game.js
+++ js/game.js
@@ -112,7 +112,7 @@ $(document).ready(function(){
 		{
 		   //lert("通关失败");
 		   $('#game1').css('display', 'none');
-		   $('#fail').css('display', 'block');
+		   $('#home1').css('display', 'block');
 		}
 	
 	
